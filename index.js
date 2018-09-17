36050113452d0538cb024b51baf244385e4486e2
Manny Shapir <mshapir95@gmail.com>
2018-09-17 12:34:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function theBeatlesPlay(musicians, instruments){
   var array1 = []
   for (var i=0; i<musicians.length; i++){
     var string = musicians[i] + " plays " + instruments[i]
-    console.log(string)
     array1[i] = string 
   }
 }
